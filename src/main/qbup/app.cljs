07afe58eae977e6dad0a1a7eded96ad2861877ff
Rick Moynihan <rick.m@swirrl.com>
2023-02-06 17:02:13 +0000

    Navigate over revisions

--- src/main/qbup/app.cljs
+++ src/main/qbup/app.cljs
@@ -69,51 +69,41 @@
                 :comp-hash (hash-components schema row)))
        table-data))
 
-(defn apply-revision [{:keys [schema history] :as tl-state}]
-  )
+(defn make-rows [& row-data]
+  (set (hash-rows default-schema row-data)))
 
-
-(def example-history [{:append
-                       #{{:area "W06000022" :period "2004-01-01T00:00:00/P3Y" :sex "Male" :life-expectancy 76.7}
-                         {:area "W06000022" :period "2004-01-01T00:00:00/P3Y" :sex "Female" :life-expectancy 80.7}}
+(def example-history [{:append (make-rows {:area "W06000022" :period "2004-01-01T00:00:00/P3Y" :sex "Male" :life-expectancy 76.7}
+                                          {:area "W06000022" :period "2004-01-01T00:00:00/P3Y" :sex "Female" :life-expectancy 80.7})
                        :comment "Add initial data"}
 
                       {:append
-                       #{{:area "W06000022" :period "2005-01-01T00:00:00/P3Y" :sex "Male" :life-expectancy 77.1}
-                         {:area "W06000022" :period "2005-01-01T00:00:00/P3Y" :sex "Female" :life-expectancy 80.9}}
+                       (make-rows {:area "W06000022" :period "2005-01-01T00:00:00/P3Y" :sex "Male" :life-expectancy 77.1}
+                                  {:area "W06000022" :period "2005-01-01T00:00:00/P3Y" :sex "Female" :life-expectancy 80.9})
                        :comment "Add 2005 data"}
 
-                      {:append #{{:area "mistake" :period "2005-01-01T00:00:00/P3Y" :sex "Male" :life-expectancy 77.1}}
+                      {:append (make-rows {:area "mistake" :period "2005-01-01T00:00:00/P3Y" :sex "Male" :life-expectancy 77.1})
                        :comment "mistake accidentally added"}
 
-                      {:delete #{{:area "mistake" :period "2005-01-01T00:00:00/P3Y" :sex "Male" :life-expectancy 77.1}}
-                       :comment "remove accidentally added"}])
+                      {:delete (make-rows {:area "mistake" :period "2005-01-01T00:00:00/P3Y" :sex "Male" :life-expectancy 77.1})
+                       :comment "Delete mistaken observation entry"}])
 
-(defn replay-changes [history]
+(defn replay-changes
+  "Returns a lazy sequence of reductions showing the state of the
+  table at every revision"
+  [history]
   (reductions
    (fn [current-table {:keys [append delete amend]}]
      (cond
        append (set/union current-table append)
        delete (set/difference current-table delete)
        amend current-table ;; TODO
-       )) #{} history))
-
-(replay-changes example-history)
-
+       ))
+   #{} history))
 
 
-(def initial-data (sort-by (apply juxt (component-schema default-schema))
-                           (hash-rows
-                            default-schema
-                            #{{:area "W06000022" :period "2004-01-01T00:00:00/P3Y" :sex "Male" :life-expectancy 76.7}
-                              {:area "W06000022" :period "2004-01-01T00:00:00/P3Y" :sex "Female" :life-expectancy 80.7}
-                              {:area "W06000022" :period "2005-01-01T00:00:00/P3Y" :sex "Male" :life-expectancy 77.1}
-                              {:area "W06000022" :period "2005-01-01T00:00:00/P3Y" :sex "Female" :life-expectancy 80.9}
-                              {:area "W06000022" :period "2006-01-01T00:00:00/P3Y" :sex "Male" :life-expectancy 77.0}})))
 
 (def init-release
-  {:revisions [:r3 :r2 :r1]
-   :current-version :r3
+  {:selected-revision 4
 
    :all-schemas [:default :update]
 
@@ -122,7 +112,9 @@
 
    :selected-schema default-schema
 
-   :current-table initial-data})
+   :history example-history
+
+   })
 
 (def init-state
   init-release)
@@ -130,13 +122,34 @@
 (def state (r/atom init-state))
 
 (defn revisions []
-  (let [revisions (:revisions @state)
+  (let [num-revisions (count (:history @state))
+        revisions (range num-revisions 0 -1)
         fst-rev (first revisions)]
-    [:select {:name "revision"}
-     (for [r revisions]
-       [:option {:value r :key r} (if (= fst-rev r)
-                                    (str r " (latest)")
-                                    r)])]))
+    [:select {:name "revision"
+              :default-value (:selected-revision @state)
+              :on-change #(swap! state assoc :selected-revision (-> % .-target .-value))}
+     (doall
+      (for [r revisions]
+        [:option {:value r
+                  :key r}
+         (if (= fst-rev r)
+           (str r " (latest)")
+           r)]))]))
+
+(comment
+
+  (hash-rows
+            default-schema
+            #{{:area "W06000022" :period "2004-01-01T00:00:00/P3Y" :sex "Male" :life-expectancy 76.7}
+              {:area "W06000022" :period "2004-01-01T00:00:00/P3Y" :sex "Female" :life-expectancy 80.7}
+              {:area "W06000022" :period "2005-01-01T00:00:00/P3Y" :sex "Male" :life-expectancy 77.1}
+              {:area "W06000022" :period "2005-01-01T00:00:00/P3Y" :sex "Female" :life-expectancy 80.9}
+              {:area "W06000022" :period "2006-01-01T00:00:00/P3Y" :sex "Male" :life-expectancy 77.0}})
+  )
+
+(defn sort-observations [row-set]
+  (sort-by (apply juxt (component-schema default-schema))
+           row-set))
 
 (defn table []
   [:table
@@ -151,8 +164,10 @@
           [:td.datatype {:key name} [:em (str "(" (clojure.core/name datatype) ")")]])]]
 
       [:tbody
-       (let [data (:current-table @state)]
-         (for [row data]
+       (let [{:keys [history selected-revision] :as state} @state
+             current-table (nth (replay-changes history)
+                                (dec selected-revision))]
+         (for [row current-table]
            ^{:key row}
            [:tr
             (for [coll-id (map :name columns)
